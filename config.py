b2b63cf6c0da506ef293929958a96d003b27b5d2
Dmitrii <yndx-dvornikoff@yandex.ru>
2021-11-28 15:46:16 +0300

    Exp. 2
    Эксперимент: включать в обучающую выборку документы из начала и конца оригинального (неотсорированного и несбалансированного), используя таким образом алгоритм для "уточнения" встроенного в Semantic Scholar Corpus метода оценки документов, на основе которого сортируется поисковая выдача.

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from frozendict import frozendict
+
 encoding = 'utf-8'
 
 rel_path_0_papers_json = 'src/0_papers.json'
@@ -37,6 +39,20 @@ k = 5
 
 best_params_ms_json = 'res/best_params_ms.json'
 best_params_txt = 'res/best_params.txt'
+
+# Варианты альф
+alphas = (1, 0.8, 0.5)
+
+best_alpha = alphas[2]
+
+# Варианты весов
+ws = (
+    # Слова, авторы, журналы, области
+    frozendict(zip(set_names, (1, 1, 1, 1))),
+    frozendict(zip(set_names, (1, 4, 3, 2)))
+)
+
+best_w = ws[1]
 
 max_vol_frac = 0.8
 min_vol = 5
---
+++ five_dive.py
@@ -0,0 +1,38 @@
+import config as c
+from data_preparing.shuffle_n_sort import shuffle
+from data_preparing.balancing import do_balance
+from data_preparing.dict_list_worker import to_list_of_dicts_of_series
+from naive_bayes_mod import NaiveBayesMod
+from metrics import calc_metrics
+
+import math
+import time
+import pandas as pd
+
+dfs = dict(zip([s for s in c.set_names], [pd.read_csv(f'{c.data_prev}{s}.csv') for s in c.set_names]))
+# dfs_shuffled_balanced = do_balance(shuffle(dfs, random_state=1), c.field_class_, c.classes)
+
+border_indexes = (10, 9, 8, 7, 6, 5, 4, 3, 2)
+for border_index in border_indexes:
+    print(f'Объъём обучающей выборки: {border_index * 2}')
+    train_dfs, test_dfs = dict(zip([type_ for type_ in dfs.keys()],
+                                   [df.loc[:border_index]
+                                   # .append(df.loc[math.floor(len(df)/2)-border_index+1:math.floor(len(df)/2)])
+                                   .append(df.loc[len(df)-border_index+1:]) for df in dfs.values()])), \
+                          dict(zip([type_ for type_ in dfs.keys()],
+                                   [df.loc[border_index+1:len(df)-border_index]
+                                   # .append(df.loc[math.floor(len(df)/2)+1:len(df)-border_index])
+                                    for df in dfs.values()]))
+    naive_bayes = NaiveBayesMod()
+    train_Xs, train_ys = dict([(type_, df.drop(columns=[c.field_id_, c.field_class_]))
+                               for type_, df in train_dfs.items()]), \
+                         list(train_dfs.values())[0][c.field_class_].to_list()
+    naive_bayes.fit(train_Xs, train_ys)
+    naive_ys, true_ys = [naive_bayes.classify(X)
+                         for X in to_list_of_dicts_of_series(dict([(type_, df.drop(columns=[c.field_id_, c.field_class_]))
+                                                                   for type_, df in test_dfs.items()]))], \
+                        list(test_dfs.values())[0][c.field_class_].to_list()
+    metrics = calc_metrics(naive_ys, true_ys)
+    for metric_name, value in metrics.items():
+        print(f'{metric_name}:\t{value}')
+    print('--------------------------')
