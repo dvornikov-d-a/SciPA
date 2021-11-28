b7a895868c3ffaa3bcf516734b735ba8bc3daab6
Dmitrii <yndx-dvornikoff@yandex.ru>
2021-11-28 14:29:05 +0300

    Exp. 1 Fix
    Исправлена ошибка отображения графиков

--- sci_pa_plot.py
+++ sci_pa_plot.py
@@ -18,24 +18,24 @@ for vol, ms in vol_ms.items():
             ys[ms_name][metric_name].append(point)
 
 colors = ('r', 'g', 'b', 'k')
-# for ms_name, metrics in ys.items():
-#     fig, ax = plt.subplots()
-#     plt.xlabel('train dataset volume')
-#     plt.ylabel(f'{ms_name} values')
-#     for i, (metric_name, points) in enumerate(metrics.items()):
-#         plt.plot(xs, points, colors[i], label=metric_name)
-#     ax.legend()
-#     plt.show()
-#     plt.savefig(f'res/plots/whole_{ms_name}s.png')
+for ms_name, metrics in ys.items():
+    fig, ax = plt.subplots()
+    plt.xlabel('train dataset volume')
+    plt.ylabel(f'{ms_name} values')
+    for i, (metric_name, points) in enumerate(metrics.items()):
+        plt.plot(xs, points, colors[i], label=metric_name)
+    ax.legend()
+    # plt.show()
+    plt.savefig(f'res/plots/whole_{ms_name}s.png')
 
-for max_vol_i in [int(vol / c.min_vol) for vol in (150, 100, 50, 25)]:
-    for ms_name, metrics in ys.items():
-        fig, ax = plt.subplots()
-        plt.xlabel('train dataset volume')
-        plt.ylabel(f'{ms_name} values')
-        for i, (metric_name, points) in enumerate(metrics.items()):
-            plt.plot(xs[-max_vol_i:], points[-max_vol_i:], colors[i], label=metric_name)
-        ax.legend()
-        # plt.show()
-        plt.savefig(f'res/plots/max_vol_{max_vol_i * c.min_vol}_{ms_name}s.png')
+# for max_vol_i in [int(vol / c.min_vol) for vol in (150, 100, 50, 25)]:
+#     for ms_name, metrics in ys.items():
+#         fig, ax = plt.subplots()
+#         plt.xlabel('train dataset volume')
+#         plt.ylabel(f'{ms_name} values')
+#         for i, (metric_name, points) in enumerate(metrics.items()):
+#             plt.plot(xs[-max_vol_i:], points[-max_vol_i:], colors[i], label=metric_name)
+#         ax.legend()
+#         # plt.show()
+#         plt.savefig(f'res/plots/max_vol_{max_vol_i * c.min_vol}_{ms_name}s.png')
 
